84b994efdb7bbff8112d610f9239eef27abd693a
JacksonBey <jacksongrowson@gmail.com>
2023-09-25 10:35:39 -0700

    got image crop and rotate working

--- uploader-example-code/example/index.js
+++ uploader-example-code/example/index.js
@@ -63,49 +63,7 @@ function handleDrop(e) {
       img.addEventListener('click', function() {
         openModal();
       });
-
-      // // Add rotation button for each image
-      // let rotateBtn = document.createElement('button');
-      // rotateBtn.innerHTML = 'Rotate';
-      // rotateBtn.onclick = function() {
-      //   rotateImage(img);
-      // };
-      // document.getElementById('thumbnails').appendChild(rotateBtn);
-
-      // // Add cropping button for each image (For demonstration)
-      // let cropBtn = document.createElement('button');
-      // cropBtn.innerHTML = 'Crop';
-      // cropBtn.onclick = function() {
-      //   cropImage(img);
-      // };
-      // document.getElementById('thumbnails').appendChild(cropBtn);
-
-      // // Open editor modal and display image
-      // const canvas = document.getElementById('editorCanvas');
-      // const ctx = canvas.getContext('2d');
-      // canvas.width = img.width;
-      // canvas.height = img.height;
-      // img.onload = function() {
-      //   ctx.drawImage(img, 0, 0, img.width, img.height);
-      // };
-      // openModal();
     };
   });
 
 }
-
-// import Cropper from 'cropperjs';
-
-// const image = document.getElementById('image');
-// const cropper = new Cropper(image, {
-//   aspectRatio: 16 / 9,
-//   crop(event) {
-//     console.log(event.detail.x);
-//     console.log(event.detail.y);
-//     console.log(event.detail.width);
-//     console.log(event.detail.height);
-//     console.log(event.detail.rotate);
-//     console.log(event.detail.scaleX);
-//     console.log(event.detail.scaleY);
-//   },
-// });
